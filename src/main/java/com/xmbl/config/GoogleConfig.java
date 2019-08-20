72dd8e28edcacb9db63cb2c1775a5bbcec377d08
noseparte <noseparte@aliyun.com>
2019-08-20 17:23:12 +0800

    payment springboot版本

---
+++ src/main/java/com/xmbl/config/GoogleConfig.java
@@ -0,0 +1,28 @@
+package com.xmbl.config;
+
+/**
+ * Copyright © 2018 noseparte © BeiJing BoLuo Network Technology Co. Ltd.
+ *
+ * @Author Noseparte
+ * @Compile --
+ * @Version 1.0
+ * @Description
+ */
+public class GoogleConfig {
+
+    // 应用网关
+    public final static String CLIENT_ID = "";
+
+    public final static String ACCOUNT_ID = "";
+
+    public final static String CLIENT_SECRET = "Cf1FZbFZHWlxp63VTWDVqlXD";
+
+    public final static String REDIRECT_URI = "http://pt.ugcapp.com:8082/api/pay/google_redirect_url";
+
+    public final static String SSL_JKS_PATH = "/data/xmbl/key/fkcz-ce4f763ca609.p12";
+//    public final static String SSL_JKS_PATH = "D:/data/key/fkcz-ce4f763ca609.p12";
+
+    public final static String PRIVATE_KEY = "ce4f763ca609eedfabec7364d28ddbdab3afe299";
+
+
+}
